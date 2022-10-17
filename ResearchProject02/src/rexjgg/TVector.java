40523ce4bbb64df516ed9017717a555fd8d4346a
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-17 10:10:12 +0900

    format

--- ResearchProject02/src/rexjgg/TVector.java
+++ ResearchProject02/src/rexjgg/TVector.java
@@ -1,4 +1,5 @@
 package rexjgg;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -24,7 +25,9 @@ public class TVector {
 
 	/**
 	 * コピーコンストラクタ
-	 * @param src コピー元
+	 * 
+	 * @param src
+	 *              コピー元
 	 */
 	public TVector(TVector src) {
 		fData = new double[src.fData.length];
@@ -35,7 +38,9 @@ public class TVector {
 
 	/**
 	 * 自分自身へコピーする．
-	 * @param src コピー元
+	 * 
+	 * @param src
+	 *              コピー元
 	 * @return 自分自身
 	 */
 	public TVector copyFrom(TVector src) {
@@ -50,6 +55,7 @@ public class TVector {
 
 	/*
 	 * (非 Javadoc)
+	 * 
 	 * @see java.lang.Object#clone()
 	 */
 	@Override
@@ -59,7 +65,9 @@ public class TVector {
 
 	/**
 	 * ストリームへ書き出す．
-	 * @param pw 出力ストリーム
+	 * 
+	 * @param pw
+	 *             出力ストリーム
 	 */
 	public void writeTo(PrintWriter pw) {
 		pw.println(fData.length);
@@ -71,7 +79,9 @@ public class TVector {
 
 	/**
 	 * ストリームから読み込む．
-	 * @param br 入力ストリーム
+	 * 
+	 * @param br
+	 *             入力ストリーム
 	 * @throws IOException
 	 */
 	public void readFrom(BufferedReader br) throws IOException {
@@ -85,7 +95,9 @@ public class TVector {
 
 	/**
 	 * 次元数を設定する．
-	 * @param dimension 次元数
+	 * 
+	 * @param dimension
+	 *                    次元数
 	 */
 	public void setDimension(int dimension) {
 		if (fData.length != dimension) {
@@ -95,6 +107,7 @@ public class TVector {
 
 	/**
 	 * 次元数を返す．
+	 * 
 	 * @return 次元数
 	 */
 	public int getDimension() {
@@ -103,7 +116,9 @@ public class TVector {
 
 	/**
 	 * 要素を返す．
-	 * @param index インデックス
+	 * 
+	 * @param index
+	 *                インデックス
 	 * @return 要素
 	 */
 	public double getElement(int index) {
@@ -112,8 +127,11 @@ public class TVector {
 
 	/**
 	 * 要素を設定する．
-	 * @param index インデックス
-	 * @param e 要素の値
+	 * 
+	 * @param index
+	 *                インデックス
+	 * @param e
+	 *                要素の値
 	 */
 	public void setElement(int index, double e) {
 		fData[index] = e;
@@ -124,12 +142,14 @@ public class TVector {
 		String str = "";
 		for (int i = 0; i < fData.length; ++i) {
 			str += fData[i] + " ";
-		};
+		}
+		;
 		return str;
 	}
 
 	/*
 	 * (非 Javadoc)
+	 * 
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
@@ -146,7 +166,9 @@ public class TVector {
 
 	/**
 	 * ベクトルを足す．
-	 * @param v ベクトル
+	 * 
+	 * @param v
+	 *            ベクトル
 	 * @return 自分自身
 	 */
 	public TVector add(TVector v) {
@@ -159,7 +181,9 @@ public class TVector {
 
 	/**
 	 * ベクトルを引く．
-	 * @param v ベクトル
+	 * 
+	 * @param v
+	 *            ベクトル
 	 * @return 自分自身
 	 */
 	public TVector subtract(TVector v) {
@@ -172,7 +196,9 @@ public class TVector {
 
 	/**
 	 * スカラー倍する．
-	 * @param a スカラー値
+	 * 
+	 * @param a
+	 *            スカラー値
 	 * @return 自分自身
 	 */
 	public TVector scalarProduct(double a) {
@@ -185,7 +211,9 @@ public class TVector {
 
 	/**
 	 * 要素ごとに割る．
-	 * @param v ベクトル
+	 * 
+	 * @param v
+	 *            ベクトル
 	 * @return 自分自身
 	 */
 	public TVector elementwiseDevide(TVector v) {
@@ -199,7 +227,9 @@ public class TVector {
 
 	/**
 	 * 要素ごとに掛ける．
-	 * @param v ベクトル
+	 * 
+	 * @param v
+	 *            ベクトル
 	 * @return 自分自身
 	 */
 	public TVector elementwiseProduct(TVector v) {
@@ -213,7 +243,9 @@ public class TVector {
 
 	/**
 	 * 内積値を返す．
-	 * @param v ベクトル
+	 * 
+	 * @param v
+	 *            ベクトル
 	 * @return 内積値
 	 */
 	public double innerProduct(TVector v) {
@@ -227,6 +259,7 @@ public class TVector {
 
 	/**
 	 * L2ノルムを返す．
+	 * 
 	 * @return L2ノルム
 	 */
 	public double calculateL2Norm() {
@@ -239,6 +272,7 @@ public class TVector {
 
 	/**
 	 * 正規化する．
+	 * 
 	 * @return 自分自身
 	 */
 	public TVector Normalize() {
